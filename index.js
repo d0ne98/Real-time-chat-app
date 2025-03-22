ceb48d2a92590c7026231ef993a5132fe71babeb
D0ne <d0ne@d0ne.com>
2025-03-22 08:30:33 +0000

    socket.io add

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 import express from 'express';
-
+import http from 'http';
+import { Server } from 'socket.io';
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 const port = 3000;
 
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+})
 
 
 
 
-
-
-
-app.listen(port, () =>{
-    console.log(`Server is running on port ${port}`);
+server.listen(port, () =>{
+    console.og(`Server is running on port ${port}`);
 } )
